b375d0476c4d8d6840c86c60924adfd3c67c6210
Bob <kohei.suzuki808@gmail.com>
2020-06-28 18:41:36 +0900

    Created gen() and video_feed to get livestream via webcam

--- app.py
+++ app.py
@@ -1,17 +1,26 @@
-from flask import Flask, render_template
-from lib.ModelProcess import ModelProcess
-app = Flask(__name__)
+from flask import Flask, render_template, Response
+from lib.Process import VideoCamera
 
-model_path = "save_model/keras_digit_model.h5"
-mp = ModelProcess(model_path)
-model = mp.get_model()
+app = Flask(__name__)
 
 
 @app.route('/')
-def hello():
-    name = model.summary()
-    return render_template("index.html", name=name)
+def index():
+    return render_template('index.html')
 
 
-if __name__ == "__main__":
-    app.run(debug=True)
+def gen(camera):
+    while True:
+        frame = camera.get_frame()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(gen(VideoCamera()),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True)
