13f4e5e1440020afbc4843f975c86ac7b5878954
Bob <kohei.suzuki808@gmail.com>
2020-07-06 18:39:55 +0900

    Created a new root, /solve, and define a const, N

--- app.py
+++ app.py
@@ -1,31 +1,52 @@
-from flask import Flask, render_template, Response
+from flask import Flask, render_template, Response, request
 from lib.Process import VideoCamera
 from lib.ModelProcess import ModelProcess
+from lib.Sudoku import Sudoku
 
-
+N = 9
 app = Flask(__name__)
 mp = ModelProcess("save_model/Digit_Recognizer.h5")
 model = mp.get_model()
+sudoku = Sudoku(N)
 
 
-@app.route('/')
+@app.route('/', methods=["GET", "POST"])
 def index():
-    return render_template('index.html')
+    grid = sudoku.get_grid()
+    return render_template('index.html', grid=grid)
 
 
-def gen(camera):
-    while True:
-        frame, return_grid = camera.sudoku_cv()
-        if return_grid is not None:
-            yield ""
+@app.route('/solve', methods=["GET", "POST"])
+def solve():
+    if request.method == "POST":
+        fixed_nums_flatten = \
+            [int(digit) for digit in request.form.getlist("name")]
+        fixed_grid = sudoku.create_grid_from_list(fixed_nums_flatten)
+        status, solution = sudoku.solve(fixed_grid)
+        if status:
+            return render_template('index.html',
+                                   grid=fixed_grid,
+                                   solution=solution)
         else:
-            yield (b'--frame\r\n'
-                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+            return render_template('index.html',
+                                   grid=fixed_grid,
+                                   solution=fixed_grid)
+    else:  # ERROR: nerve called without clicking solve button
+        grid = sudoku.get_grid()
+        return render_template('index.html', grid=grid)
 
 
 @app.route('/video_feed')
 def video_feed():
-    return Response(gen(VideoCamera(model)),
+    def gen(camera):
+        while True:
+            frame, grid = camera.sudoku_cv()
+            if grid is not None:
+                pass
+            else:
+                yield (b'--frame\r\n'
+                       b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+    return Response(gen(VideoCamera(model, sudoku)),
                     mimetype='multipart/x-mixed-replace; boundary=frame')
 
 
